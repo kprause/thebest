537a3acc5475477617739ef0a9e47b5ba3d01290
m <mn@klkl.de>
2017-05-29 10:32:45 +0200

    added read conf file (readFileByLine)

--- desktop-client/src/main/java/Lib.java
+++ desktop-client/src/main/java/Lib.java
@@ -1,9 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Lib {
 
-	public static ArrayList<String> readFileByLine(String path){
-		return null;
+	public static ArrayList<String> readFileByLine(String path)throws IOException {	
+			
+		ArrayList<String> lines = new ArrayList<String>();
+		FileInputStream fis = new FileInputStream(path);
+		 
+			//Construct BufferedReader from InputStreamReader
+			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+		 
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				lines.add(line);
+			}
+			br.close();
+			
+			return lines;
 	}
 	
 	
